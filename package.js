bd7055a74a3cf37d973a5e32169b9e95f3fb610f
Lancelot <hogsleigh@gmail.com>
2016-08-31 21:57:11 +0300

    Update package.js

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   summary: "HIPAA audit log for ClinicalFramework.",
   version: "2.4.2",
-  git: "http://github.com/clinical-meteor/clinical-hipaa-audit-log.git",
+  git: "https://github.com/lancelotsmith/hipaa-audit-log.git",
   name: "clinical:hipaa-audit-log"
 });
 
